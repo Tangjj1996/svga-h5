866b061e511a922066dac753faa24259400364f2
Tangjj1996 <Tangjj_1996@163.com>
2020-11-18 10:23:40 +0800

    feat: bundle package

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-// import SVGA from 'svgaplayerweb';
+import SVGA from 'svgaplayerweb';
 
-// const player = new SVGA.Player('#');
+const player = new SVGA.Player('#qqCanvas');
+const parser = new SVGA.Parser('#qqCanvas');
 
-console.log(111)
+parser.load('asset/test.svga', function (videoItem) {
+  player.setVideoItem(videoItem);
+  player.startAnimation();
+});
